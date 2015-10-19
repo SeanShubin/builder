fe756cb6582eaf7df0948371dfb87f52c02970bd
Sean Shubin <seanshubin@gmail.com>
2015-10-18 17:36:34 -0700

    Decoupled github directory from user home

--- console/src/main/scala/com/seanshubin/builder/console/RunnerWiring.scala
+++ console/src/main/scala/com/seanshubin/builder/console/RunnerWiring.scala
@@ -13,7 +13,6 @@ trait RunnerWiring {
   def configuration: Configuration
 
   lazy val userHome = System.getProperty("user.home")
-  lazy val homeDirectory:Path = Paths.get(userHome).resolve(Paths.get("github", "sean"))
   lazy val emitLine: String => Unit = println
   lazy val devonMarshaller: DevonMarshaller = DevonMarshallerWiring.Default
   lazy val fileSystem: FileSystemIntegration = new FileSystemIntegrationImpl
@@ -21,13 +20,14 @@ trait RunnerWiring {
   lazy val reporter: Reporter = new ReporterImpl(configuration.reportDirectory, fileSystem, devonMarshaller, charset)
   lazy val notifications: Notifications = new LineEmittingNotifications(devonMarshaller, emitLine)
   lazy val environment: Settings = configuration.settingsByUserHome(userHome)
+  lazy val githubDirectory:Path = environment.githubDirectory
   lazy val emitProcessLine: String => Unit = notifications.execOutput
   lazy val processLoggerFactory: ProcessLoggerFactory = new ProcessLoggerFactoryImpl(emitProcessLine)
   lazy val systemExecutor: SystemExecutor = new SystemExecutorImpl(processLoggerFactory)
   lazy val jsonMarshaller: JsonMarshaller = new JsonMarshallerImpl
   lazy val httpSender: HttpSender = new HttpSender
   lazy val githubApi: GithubApi = new GithubApiImpl(httpSender, jsonMarshaller)
-  lazy val systemApi: SystemApi = new SystemApiImpl(systemExecutor, homeDirectory, environment, notifications)
+  lazy val systemApi: SystemApi = new SystemApiImpl(systemExecutor, githubDirectory, environment, notifications)
   lazy val api: Api = new ApiImpl(systemApi, githubApi, configuration.githubUserName)
   lazy val systemClock: SystemClock = new SystemClockImpl()
   lazy val timer: Timer = new TimerImpl(systemClock)
--- core/src/main/scala/com/seanshubin/builder/core/Settings.scala
+++ core/src/main/scala/com/seanshubin/builder/core/Settings.scala
@@ -1,6 +1,9 @@
 package com.seanshubin.builder.core
 
+import java.nio.file.Path
+
 case class Settings(commandPrefix: Seq[String],
+                    githubDirectory: Path,
                     directoryListingCommand: Seq[String],
                     shouldUpgradeDependencies: Boolean,
                     mavenSettings: Option[String])
