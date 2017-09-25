311f4cd2074ffcd39859d5f95ac0912fffda56f3
SeanShubin <seanshubin@gmail.com>
2017-09-24 22:26:00 -0700

    prototyping

--- domain/src/main/scala/com/seanshubin/builder/domain/PrototypeApp.scala
+++ domain/src/main/scala/com/seanshubin/builder/domain/PrototypeApp.scala
@@ -2,10 +2,12 @@ package com.seanshubin.builder.domain
 
 import java.util.concurrent._
 
-import akka.typed.{ActorContext, ActorSystem, Behavior, Signal}
+import akka.typed.{ActorContext, ActorRef, ActorSystem, Behavior, Signal}
+import com.seanshubin.builder.domain.PrototypeApp.Event.{Found, Processed, Ready}
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext, Future, Promise}
+import scala.util.{Success, Try}
 
 object PrototypeApp extends App {
   /*
@@ -38,38 +40,17 @@ object PrototypeApp extends App {
             throw ex
         }
       }
-
     }
   }
 
-  def doSearch(): Future[Seq[String]] = futureRunner.runInFuture(Seq("found-1", "found-2"))
-
-  def process(name: String): Future[Int] = futureRunner.runInFuture(0)
-
-  sealed trait State {
-    def isDone: Boolean = false
-
-    def update(name: String, status: Int): State = ???
-
-    def results: Map[String, Int] = ???
+  def createStateMap(names: Seq[String]): Map[String, Option[Int]] = {
+    val emptyStateMap = Map[String, Option[Int]]()
+    val withBuild = names.foldLeft(emptyStateMap)(addBuild)
+    withBuild
   }
 
-  object State {
-
-    case object Initial extends State
-
-    case object Searching extends State
-
-    case class Processing(status: Map[String, Option[Int]]) extends State {
-      override def isDone: Boolean = {
-        def isNameDone(name: String, result: Option[Int]): Boolean = {
-          result.isDefined
-        }
-
-        status.forall((isNameDone _).tupled)
-      }
-    }
-
+  def addBuild(stateMap: Map[String, Option[Int]], build: String): Map[String, Option[Int]] = {
+    stateMap + (build -> None)
   }
 
   sealed trait Event
@@ -81,6 +62,87 @@ object PrototypeApp extends App {
     case class Found(names: Seq[String]) extends Event
 
     case class Processed(name: String, result: Int) extends Event
+
+  }
+
+  case class ProcessResult(name: String, result: Int)
+
+  trait Dispatcher {
+    def doSearch(): Future[Seq[String]]
+
+    def process(name: String): Future[ProcessResult]
+  }
+
+  class DispatcherImpl extends Dispatcher {
+    override def doSearch(): Future[Seq[String]] = futureRunner.runInFuture(Seq("found-1", "found-2"))
+
+    override def process(name: String): Future[ProcessResult] = futureRunner.runInFuture(ProcessResult(name, 0))
+  }
+
+  sealed trait State {
+    def isDone: Boolean = false
+
+    def update(name: String, status: Int): State = ???
+
+    def results: Map[String, Int] = ???
+
+    def handle(event: Event, dispatcher: Dispatcher, actorRef: ActorRef[Event]): State = {
+      throw new UnsupportedOperationException(this.getClass.getName)
+    }
+  }
+
+  class DispatchResultHandler(actorRef: ActorRef[Event]) {
+    def searchResult(result: Try[Seq[String]]): Unit = {
+      result match {
+        case Success(names) => actorRef ! Found(names)
+      }
+    }
+
+    def processResult(result: Try[ProcessResult]): Unit = {
+      result match {
+        case Success(ProcessResult(name, code)) => actorRef ! Processed(name, code)
+      }
+    }
+  }
+
+  object State {
+
+    case object Initial extends State {
+      override def handle(event: Event, dispatcher: Dispatcher, actorRef: ActorRef[Event]): State = {
+        val handler = new DispatchResultHandler(actorRef)
+        event match {
+          case Ready =>
+            dispatcher.doSearch().onComplete(handler.searchResult)
+            State.Searching
+        }
+      }
+    }
+
+    case object Searching extends State {
+      override def handle(event: Event, dispatcher: Dispatcher, actorRef: ActorRef[Event]): State = {
+        val handler = new DispatchResultHandler(actorRef)
+        event match {
+          case Found(names) => names.foreach(dispatcher.process(_).onComplete(handler.processResult))
+            val stateMap = createStateMap(names)
+            State.Processing(stateMap)
+        }
+      }
+    }
+
+    case class Processing(status: Map[String, Option[Int]]) extends State {
+
+      override def handle(event: Event, dispatcher: Dispatcher, actorRef: ActorRef[Event]): State = {
+
+      }
+
+      override def isDone: Boolean = {
+        def isNameDone(name: String, result: Option[Int]): Boolean = {
+          result.isDefined
+        }
+
+        status.forall((isNameDone _).tupled)
+      }
+    }
 
   }
 
@@ -148,7 +210,7 @@ object PrototypeApp extends App {
     names.map(name => name -> None).toMap
   }
 
-  class MyBehavior(eventHandler: EventHandler, done: Promise[Unit]) extends Behavior[Event] {
+  class MyBehavior(dispatcher: Dispatcher, done: Promise[Unit]) extends Behavior[Event] {
     private var state: State = State.Initial
 
     override def management(ctx: ActorContext[Event], msg: Signal): Behavior[Event] = {
@@ -158,29 +220,31 @@ object PrototypeApp extends App {
 
     override def message(ctx: ActorContext[Event], msg: Event): Behavior[Event] = {
       println(s"message($msg)")
-      msg match {
-        case Event.Ready =>
-          state = State.Searching
-          Action.Search.execute(eventHandler)
-        case Event.Found(names) =>
-          val initialProcessingStatus = createInitialProcessingStatus(names)
-          state = State.Processing(initialProcessingStatus)
-          names.foreach(Action.Process(_).execute(eventHandler))
-        case Event.Processed(name, result) =>
-          state = state.update(name, result)
-          Action.Update(name, result)
-      }
-      if (state.isDone) {
-        Action.Report(state.results)
-        done.success(())
-      }
+      state = state.handle(msg, dispatcher, ctx.getSelf())
+      //      msg match {
+      //        case Event.Ready =>
+      //          state = State.Searching
+      //          Action.Search.execute(eventHandler)
+      //        case Event.Found(names) =>
+      //          val initialProcessingStatus = createInitialProcessingStatus(names)
+      //          state = State.Processing(initialProcessingStatus)
+      //          names.foreach(Action.Process(_).execute(eventHandler))
+      //        case Event.Processed(name, result) =>
+      //          state = state.update(name, result)
+      //          Action.Update(name, result)
+      //      }
+      //      if (state.isDone) {
+      //        Action.Report(state.results)
+      //        done.success(())
+      //      }
       this
     }
   }
 
   val done: Promise[Unit] = Promise()
   val eventHandler: EventHandler = new EventHandlerImpl
-  val behavior: Behavior[Event] = new MyBehavior(eventHandler, done)
+  val dispatcher: Dispatcher = new DispatcherImpl
+  val behavior: Behavior[Event] = new MyBehavior(dispatcher, done)
   val actorSystem: ActorSystem[Event] = ActorSystem("behavior", behavior)
   val duration: Duration = Duration(5, TimeUnit.SECONDS)
 
