d52c7d457b9816ae6d44e6a68b84e6ed9c2cae74
Sean Shubin <sshubin@cj.com>
2017-09-25 11:10:40 -0700

    detangler configuration

--- prototype/src/main/scala/com/seanshubin/builder/prototype/LineEmittingNotifications.scala
+++ prototype/src/main/scala/com/seanshubin/builder/prototype/LineEmittingNotifications.scala
@@ -1,15 +1,10 @@
 package com.seanshubin.builder.prototype
 
-import java.io.{PrintWriter, StringWriter}
-
 import akka.typed.Signal
 
 class LineEmittingNotifications(emit: String => Unit) extends Notifications {
   override def asyncException(ex: Throwable): Unit = {
-    val stringWriter = new StringWriter()
-    val printWriter = new PrintWriter(stringWriter)
-    ex.printStackTrace(printWriter)
-    val s = stringWriter.getBuffer.toString
+    val s = ExceptionUtil.toStringWithStackTrace(ex)
     emit(s)
   }
 
