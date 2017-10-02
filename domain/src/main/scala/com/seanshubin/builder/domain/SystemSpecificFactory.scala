724dbb345f77135d1f64f6be5e7a4318a2aae678
Sean Shubin <sshubin@cj.com>
2017-10-02 16:34:03 -0700

    Removed dead code

--- domain/src/main/scala/com/seanshubin/builder/domain/SystemSpecificFactory.scala
+++
@@ -1,5 +0,0 @@
-package com.seanshubin.builder.domain
-
-trait SystemSpecificFactory {
-  def systemSpecificSettings(): SystemSpecific
-}
--- domain/src/main/scala/com/seanshubin/builder/domain/SystemSpecificFactoryImpl.scala
+++
@@ -1,7 +0,0 @@
-package com.seanshubin.builder.domain
-
-class SystemSpecificFactoryImpl(system: SystemContract, map: Map[String, SystemSpecific]) extends SystemSpecificFactory {
-  def systemSpecificSettings(): SystemSpecific = {
-    map(system.getProperty("user.home"))
-  }
-}
